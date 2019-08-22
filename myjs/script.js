8f8fbe6b1b5eb9135e7337c98e78a1447a971a18
babayagi <darsan_hirani@hotmail.com>
2019-08-23 01:34:22 +0300

    completed js for portfolio

--- myjs/script.js
+++ myjs/script.js
@@ -32,3 +32,67 @@ $(document).ready(function() {
       
   }); 
 });
+$(document).ready(function(){
+  $(".work1").hover(function(){
+    $("#port1").show();
+  });
+  $(".work1").mouseleave(function(){
+    $("#port1").hide();
+  })
+});
+$(document).ready(function(){
+  $(".work2").hover(function(){
+    $("#port2").show();
+  });
+  $(".work2").mouseleave(function(){
+    $("#port2").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work3").hover(function(){
+    $("#port3").show();
+  });
+  $(".work3").mouseleave(function(){
+    $("#port3").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work4").hover(function(){
+    $("#port4").show();
+  });
+  $(".work4").mouseleave(function(){
+    $("#port4").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work5").hover(function(){
+    $("#port5").show();
+  });
+  $(".work5").mouseleave(function(){
+    $("#port5").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work6").hover(function(){
+    $("#port6").show();
+  });
+  $(".work6").mouseleave(function(){
+    $("#port6").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work7").hover(function(){
+    $("#port7").show();
+  });
+  $(".work7").mouseleave(function(){
+    $("#port7").hide();
+  });
+});
+$(document).ready(function(){
+  $(".work8").hover(function(){
+    $("#port8").show();
+  });
+  $(".work8").mouseleave(function(){
+    $("#port8").hide();
+  });
+});
