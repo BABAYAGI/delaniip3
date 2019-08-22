9eb955bdb29e115e631318094fb4f8d989beb8cc
babayagi <darsan_hirani@hotmail.com>
2019-08-23 00:00:25 +0300

    completed js for what we do

---
+++ myjs/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("#design").mouseenter(function() {
+    $("#des").show();
+    $("#design").hide();
+    
+  });
+  $("#des").mouseleave(function() {
+    $("#design").show();
+    $("#des").hide();
+      
+  }); 
+});
+$(document).ready(function() {
+  $("#dev").hover(function() {
+    $("#deve").show();
+    $("#dev").hide();
+  });
+  $("#deve").mouseleave(function() {
+    $("#dev").show();
+    $("#deve").hide();
+      
+  }); 
+});
+$(document).ready(function() {
+  $("#producti").hover(function() {
+    $("#product").show();
+    $("#producti").hide();
+  });
+  $("#product").mouseleave(function() {
+    $("#producti").show();
+    $("#product").hide();
+      
+  }); 
+});
